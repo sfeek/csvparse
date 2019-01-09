2c88dc3c38f95cf75a617e6fd56e31550064147b
sfeek <shane.feek@gmail.com>
2019-01-09 10:12:01 -0500

    Linted

--- src/CSVtest.c
+++ src/CSVtest.c
@@ -8,32 +8,29 @@
 
 int main()
 {
-    char **parsed;
-    int numberOfFields;
-    int i;
+	char **parsed;
+	int numberOfFields;
+	int i;
+	/* Our test string to parse */
+	char *csv = "This is a field,,\"This line has a carriage\n return in it!\",Test,\"One\\Two,Three\",10,\\\"The End\\\"\n";
+	/* Show it on the screen */
+	printf ("%s\n\n", csv);
 
-    /* Our test string to parse */
-    char *csv = "This is a field,,\"This line has a carriage\n return in it!\",Test,\"One\\Two,Three\",10,\\\"The End\\\"\n";
+	/* Parse it! */
+	if (! (parsed = CSVParse (csv, &numberOfFields)))
+	{
+		printf ("String parsing failed!\n");
+		return 1;
+	}
 
-    /* Show it on the screen */
-    printf("%s\n\n", csv);
+	/* Show the parsed array */
+	for (i = 0; i < numberOfFields; i++)
+	{
+		printf ("Field %d: %s\n", i, parsed[i]);
+	}
 
-    /* Parse it! */
-    if (!(parsed = CSVParse(csv,&numberOfFields)))
-    {
-        printf("String parsing failed!\n");
-        return 1;
-    }
-
-    /* Show the parsed array */
-    for(i = 0;i < numberOfFields;i++)
-    {
-        printf("Field %d: %s\n",i,parsed[i]);
-    }
-
-    /* Clean up after ourselves */
-    cleanupStrings(parsed,numberOfFields);
-
-    /* And we are out of here! */
-    return 0;
+	/* Clean up after ourselves */
+	cleanupStrings (parsed, numberOfFields);
+	/* And we are out of here! */
+	return 0;
 }
